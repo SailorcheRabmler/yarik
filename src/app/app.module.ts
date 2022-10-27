4ed5b64e2486d6689649cba4a470a3d956744b17
sailorche <sailorche@rambler.ru>
2022-10-27 20:09:37 +0300

    dz2 fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './components/header/header.module';
+import { ProductsListModule } from './modules/products-list/products-list.module';
+import { DetailsListModule } from './modules/details-list/details-list.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule],
-	// providers: [],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HeaderModule,
+    ProductsListModule,
+    DetailsListModule],
+
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/modules/products-list/products-list.module.ts
+++ src/app/modules/products-list/products-list.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list.component';
 
+
 @NgModule({
 	declarations: [ProductsListComponent],
 	imports: [CommonModule],
